19edf1119359b90856578b095bc82dec2a221617
balazsDombovari <dbalazs1219@gmail.com>
2019-01-28 19:07:32 +0100

    profile updated. profile page shows who is logged in profile page skeleton created. need data for use

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 allowed_email = ["asd@asd.hu"]
 allowed_password = ["123"]
 
@@ -11,13 +10,16 @@ allowed_password = ["123"]
 def index():
     return render_template("index.html")
 
+
 @app.route('/news')
 def news():
     return render_template("news.html")
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+
+@app.route('/profile/<email>')
+def profile(email):
+    return render_template('profile.html', email=email)
+
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -30,7 +32,7 @@ def login():
     global allowed_password
 
     if email in allowed_email and password in allowed_password:
-        return render_template("index_innerpage.html")
+        return render_template("index_innerpage.html", email=email  )
 
 
 if __name__ == '__main__':
